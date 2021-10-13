07e82c3d17763571c02ad9ff6419792f5e60d75c
Allyson Santana <santana.allyson886@gmail.com>
2021-10-13 19:30:17 -0300

    revisão de funções de Manipulação de dados

--- php/test_create_api/request_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Content-Type: application/json');
-
-echo json_encode($response);
-exit;
-
